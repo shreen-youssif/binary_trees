080ea314b3cf54b76238725cecde035bbaf41f9e
shreen-youssif <shreenyoussif22@gmail.com>
2024-02-29 11:03:33 -0800

    120-binary_tree_is_avl.c

--- binary_trees.h
+++ binary_trees.h
@@ -62,6 +62,9 @@ bst_t *bst_remove(bst_t *root, int value);
 bst_t *bst_remove_recursive(bst_t *root, bst_t *node, int value);
 bst_t *bst_delete(bst_t *root, bst_t *node);
 bst_t *inorder_successor(bst_t *root);
+int binary_tree_is_avl(const binary_tree_t *tree);
+int is_avl_helper(const binary_tree_t *tree, int lo, int hi);
+size_t height(const binary_tree_t *tree);
 
 
 #endif /* BINARY_TREES_H */
