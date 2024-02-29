3e6e1efd1ee68c53f3128d1becb3f6394d11520c
shreen-youssif <shreenyoussif22@gmail.com>
2024-02-29 10:43:10 -0800

    104-binary_tree_rotate_right.c

--- binary_trees.h
+++ binary_trees.h
@@ -54,6 +54,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
 
